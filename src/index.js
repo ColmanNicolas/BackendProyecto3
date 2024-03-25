ac0b7a4e02a60c100c5902c07fda4ecd34feeeec
Diego Saleh <diego.alesaleh1@gmail.com>
2024-03-25 01:10:17 -0300

    Agrego userRoutes, userModel, validatesFields y db-validator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require ("mongoose");
 require("dotenv").config();
 
+
 const userRouter = require('./routes/...');
 const productRouter = require('./routes/...');
 
