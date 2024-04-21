6a48bb00e382157463ef72e139e25680458e5b35
ColmanNicolas <colman.nicolas.17@hotmail.com>
2024-04-21 11:42:16 -0300

    cambio frase secreta

--- src/helpers/generarJWT.js
+++ src/helpers/generarJWT.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const generarJWT = (id ="") => {
     return new Promise((resolve,reject)=>{
         const payload = { id };
-        jwt.sign(payload, process.env.SECRET,(error,token)=>{
+        jwt.sign(payload, process.env.SUPERSECRET,(error,token)=>{
             if(error){
                 console.log(error);
                 reject('No se pudo generar el token');
